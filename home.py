a7c67c1e17e8dc329d7d7a108744dda8f85479c7
Francesco Pinizzotto <fpinizzotto@gmail.com>
2025-02-21 13:28:29 +0100

    Prova immagine su home page e richiesta mapbox API Key

--- home.py
+++ home.py
@@ -2,12 +2,22 @@ import streamlit as st
 from utils import upload_file, process_csv_file
 import duckdb
 import time
+from PIL import Image
+
+
 
 st.set_page_config(page_title="Home - Profilo Linkedin", page_icon="📄", layout="wide")
 st.cache_data.clear() 
 
+
+
 if "duckdb_conn" in st.session_state:
     del st.session_state["duckdb_conn"]
+
+immagine = Image.open("a-high-quality-streamlit-application-hom_YAAA5HthTTiTa4Ze4j7hNw__ifsh2uiRhmYnWqNbwV6KQ.jpeg")
+st.image(immagine, caption="", use_container_width=True)
+
+# Inserisci il CSS personalizzato per impostare lo sfondo
 
 file = upload_file()
 
@@ -21,6 +31,9 @@ if file:
         context_data = context_data[nuovo_ordine]
         st.session_state["context_data"] = context_data
         st.success("File caricato con successo!")
+        # Carica l'immagine usando un percorso relativo (assicurati che il file "immagine.jpg" sia nella stessa cartella o in una sottocartella)
+
+        
     else:
         st.error("Errore durante l'elaborazione del file.")
 else:
--- pages/5 - Connections Map 2.py
+++ pages/5 - Connections Map 2.py
@@ -16,26 +16,29 @@ if "context_data" in st.session_state:
     if "Location" in context_data.columns:
         df = create_df_per_map2(context_data)
         #df = df_per_map.groupby(["Company", "Location", "Latitude","Longitude"]).size().reset_index(name="Count")
-
+        # Chiedere all'utente di inserire un testo
+        user_input = st.text_input("Please, enter your Mapbox API Key:")
+        # Visualizzare l'input solo se non è vuoto
+        if user_input:
         # Imposta il tuo access token Mapbox
-        px.set_mapbox_access_token("x")
+            px.set_mapbox_access_token(user_input)
 
-        # Creare la mappa con Mapbox
-        fig = px.scatter_mapbox(
-            df, 
-            lat="Latitude", 
-            lon="Longitude", 
-            color="Company",  # Colore diverso per ogni azienda
-            size = "Count",
-            zoom=5,  # Livello di zoom
-            mapbox_style="carto-positron",
-            hover_name="Company",
-            hover_data=["Count", "Location", "Latitude","Longitude"]  # Stile della mappa
-        )
+            # Creare la mappa con Mapbox
+            fig = px.scatter_mapbox(
+                df, 
+                lat="Latitude", 
+                lon="Longitude", 
+                color="Company",  # Colore diverso per ogni azienda
+                size = "Count",
+                zoom=5,  # Livello di zoom
+                mapbox_style="carto-positron",
+                hover_name="Company",
+                hover_data=["Count", "Location", "Latitude","Longitude"]  # Stile della mappa
+            )
 
-        fig.update_layout(dragmode="pan", showlegend=False, uirevision="static", margin={"r": 0, "t": 0, "l": 0, "b": 0})
+            fig.update_layout(dragmode="pan", showlegend=False, uirevision="static", margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
-        st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True)
     else:
         st.warning(f'''Il file corrente non ha i dati per la costruzione della mappa. 
                    Torna alla home e carica un file con i dati di geolocalizzazione''')
